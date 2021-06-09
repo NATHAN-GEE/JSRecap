cbf152e2daa1e96176a727828ff7348cfb7eb024
FirstName LastName <nathangee867@gmail.com>
2021-06-09 16:51:09 -0400

    added js file variables

---
+++ variables.js
@@ -0,0 +1,24 @@
+// create and initialize threee different variables, each with a different data type.
+//write a function that says Hello World!
+
+//my age
+let birthYear = 1986;
+let age = 2021 - birthYear;
+if (age >= 21) {
+  console.log(`You are ${age}, you are an adult!`);
+} else {
+  console.log(`You are ${age}, you need to grow up still. 😂`);
+}
+
+//my name
+var name = "Nathan Gee";
+console.log("My name is " + name);
+
+//my schoole
+const school = "Eleven Fifty Academy (EFA)";
+console.log(`The school I'm attending right now is ${school}.`);
+
+function helloWorld() {
+  console.log("Hello, World!!!");
+}
+helloWorld();
